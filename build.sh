8fa9f253cc98d1b56c5f61679133b6d7ee4c3d0b
Aneesh Durg <aneeshdurg17@gmail.com>
2026-02-24 12:00:44 -0600

    Update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
+#!/bin/bash
 ./autogen.sh
-./configure
-make clean
-make CFLAGS="-fPIC"
-# Produce squashfuse-hl.so from built objects
-gcc -shared squashfuse-hl.o .libs/*.a .libs/libsquashfuse_ll.so \
-  -lfuse3 -llzma -lzstd -llzo2 -llz4 -lxxhash -lz \
-  -o squashfuse-hl.so
-# Copy libsquashfuse_ll.so from build directory
-cp .libs/libsquashfuse_ll.so.0.0.0 libsquashfuse_ll.so
+CFLAGS="-fPIC -fPIE -DNDEBUG" ./configure
+make -j32
